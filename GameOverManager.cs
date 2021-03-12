889770f5856cd7a581fa09832cd57fbe5015cc7a
heoyongjun2 <80499442+heoyongjun2@users.noreply.github.com>
2021-03-12 11:47:56 +0900

    Add files via upload

---
+++ GameOverManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace HYJ
+{
+    public class GameOverManager : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        public void ResetLevel()
+        {
+            MyScoreManager.score = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
